8554b272c8fe754ce6ebd12c158f8071b5e12035
Yami <yamilaplatero@gmail.com>
2026-06-25 18:12:19 -0300

    arreglo ruta

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require ('dotenv').config();
 // Crear instancia de Express
-const app = require('../NuevoPI2/app.js');
+const app = require('./app');
 // Configurar puerto
 const PORT = process.env.PORT || 3000;
 
